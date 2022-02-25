eae6af6db25a1e3e43e127201198168a9be8262a
jin1125 <shogo.jinta@gmail.com>
2022-02-25 20:15:48 +0900

    96

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -31,6 +31,8 @@
                               </div>
                           </div>
 
+                          <x-select-image name="image1" />
+
                           <div class="p-2 w-full flex justify-around mt-4">
                               <button
                                   type="button"
